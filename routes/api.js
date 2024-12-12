6051feb4ed3e467a4d073e6ec4db0fec6f7ce3bf
KumaCatz <tod.kagan@gmail.com>
2024-12-12 17:41:22 -0300

    feat: base data api

--- routes/api.js
+++ routes/api.js
@@ -1,20 +1,28 @@
 'use strict';
 
-const { client }    = require('../db-connection.js');
+const { client }  = require('../db-connection.js');
 
-const database    = client.db("anonymousMessageBoard");
-const threads     = database.collection("threads");
+const database  = client.db("anonymousMessageBoard");
+const threads   = database.collection("threads");
+
+const date = new Date();
+const baseData = {
+  'text': '',
+  'created_on': date,
+  'delete_password': ''
+};
 
 module.exports = function (app) {
-  
+
   app.route('/api/threads/:board')
     .post((req, res) => {
       console.log(req.params)
       console.log(req.body)
+
     })
     .get((req, res) => {
 
-    })
+    });
   app.route('/api/replies/:board');
 
 };
