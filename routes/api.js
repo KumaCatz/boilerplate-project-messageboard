8ab525e760a4fa959a3d656be41a91894ac1ae67
KumaCatz <tod.kagan@gmail.com>
2024-12-12 17:48:44 -0300

    feat: newThread post

--- routes/api.js
+++ routes/api.js
@@ -5,19 +5,30 @@ const { client }  = require('../db-connection.js');
 const database  = client.db("anonymousMessageBoard");
 const threads   = database.collection("threads");
 
-const date = new Date();
-const baseData = {
-  'text': '',
-  'created_on': date,
-  'delete_password': ''
-};
+const getBaseData = (text, delete_password) => {
+  const date = new Date();
+
+  return {
+    'text': '',
+    'created_on': date,
+    'delete_password': ''
+  };
+}
 
 module.exports = function (app) {
 
   app.route('/api/threads/:board')
     .post((req, res) => {
+      const {text, delete_password} = req.body
       console.log(req.params)
       console.log(req.body)
+      const baseData = getBaseData(text, delete_password)
+      const newThread = {
+        ...baseData,
+        'bumped_on': date,
+        'reported': false,
+        'replies': []
+      }
 
     })
     .get((req, res) => {
